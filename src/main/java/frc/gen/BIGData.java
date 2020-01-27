74179975f730c39c5c457c03014902f9823347c6
InvincibleRMC <rmc@carlstrom.com>
2020-01-26 19:30:39 -0800

    add semiColon

--- src/main/java/frc/gen/BIGData.java
+++ src/main/java/frc/gen/BIGData.java
@@ -273,7 +273,7 @@ public class BIGData {
 	public static void updateCamera(double r, double a, double t) {
 		put("camera_azimuth", a);
 		put("camera_range", r);
-		put("relative_angle",t)
+		put("relative_angle", t);
 	}
 
 	public static void updateLidar(double r) {
