a5c59dd201f8f1efba0dfa67fb4b493428abb4b1
InvincibleRMC <rmc@carlstrom.com>
2020-01-29 16:12:00 -0800

    Added all obstacles

--- src/main/java/frc/positiontracking/fieldmap/FieldMap.java
+++ src/main/java/frc/positiontracking/fieldmap/FieldMap.java
@@ -3,47 +3,117 @@ package frc.positiontracking.fieldmap;
 import frc.positiontracking.fieldmap.geometry.Polygon;
 import frc.positiontracking.fieldmap.geometry.Vector;
 
+import frc.gen.BIGData;
+
 public class FieldMap {
 
-    private static double FIELD_WIDTH, FIELD_HEIGHT;
-    private static Vector bounds;
-    private Polygon wall;
-    private static Polygon[] obstacles;
-    private static VisionTarget[] visionTargets;
+        private static double FIELD_WIDTH, FIELD_HEIGHT;
+        private static Vector bounds;
+        private Polygon wall;
+        private static Polygon[] obstacles;
+        private static Polygon[] safeZones;
+        private static VisionTarget[] visionTargets;
+        private static final double ROBOT_WIDTH = 50;
+        private static double SHORTEST_SIDE;
 
-    public FieldMap() {
-        buildMap();
-        wall = new Polygon(new Vector(0, 0), new Vector(FIELD_HEIGHT, 0), new Vector(FIELD_HEIGHT, FIELD_WIDTH),
-                new Vector(0, FIELD_WIDTH));
-    }
+        public FieldMap() {
+                SHORTEST_SIDE = Math.min(BIGData.getDouble("robot_width"), BIGData.getDouble("robot_height"));
+                buildMap();
+                wall = new Polygon(new Vector(0, 0), new Vector(FIELD_HEIGHT, 0), new Vector(FIELD_HEIGHT, FIELD_WIDTH),
+                                new Vector(0, FIELD_WIDTH));
 
-    private static void buildMap() {
-        obstacles = new Polygon[2];
-        visionTargets = new VisionTarget[1];
-        FIELD_WIDTH = 323.31;
-        FIELD_HEIGHT = 629.25;
-        bounds = new Vector(FIELD_HEIGHT, FIELD_WIDTH);
+        }
 
-        Polygon RightSideTrench = new Polygon(new Vector(254, 320), new Vector(254, FIELD_WIDTH),
-                new Vector(285, FIELD_WIDTH), new Vector(285, 320));
-        Polygon LeftSideTrench = new Polygon(new Vector(254, 266), new Vector(285, 266), new Vector(285, 270),
-                new Vector(254, 270));
+        private static void buildMap() {
+                obstacles = new Polygon[13];
+                safeZones = new Polygon[3];
+                visionTargets = new VisionTarget[1];
+                FIELD_WIDTH = 323.31;
+                FIELD_HEIGHT = 629.25;
+                bounds = new Vector(FIELD_HEIGHT, FIELD_WIDTH);
 
-        obstacles[0] = RightSideTrench;
-        obstacles[1] = LeftSideTrench;
+                Polygon RightSideTrench = new Polygon(new Vector(254, 320), new Vector(254, FIELD_WIDTH),
+                                new Vector(285, FIELD_WIDTH), new Vector(285, 320));
+                Polygon LeftSideTrench = new Polygon(new Vector(254, 266), new Vector(285, 266), new Vector(285, 270),
+                                new Vector(254, 270));
 
-        VisionTarget farGoal = new VisionTarget(new Vector(FIELD_HEIGHT, 228.655), Math.PI, true);
+                Polygon LeftCornerNear = new Polygon(new Vector(0, 0), new Vector(25.715, 0), new Vector(0, 70.655));
+                Polygon RightCornerNear = new Polygon(new Vector(0, 253.531), new Vector(0, FIELD_WIDTH),
+                                new Vector(25.715, FIELD_WIDTH));
+                Polygon LeftCornerFar = new Polygon(new Vector(FIELD_HEIGHT, 0), new Vector(604.18, 0),
+                                new Vector(FIELD_HEIGHT, 70.655));
+                Polygon RightCornerFar = new Polygon(new Vector(FIELD_HEIGHT, 253.531),
+                                new Vector(FIELD_HEIGHT, FIELD_WIDTH), new Vector(603.545, FIELD_WIDTH));
 
-        visionTargets[0] = farGoal;
+                Polygon BlueTrench = new Polygon(new Vector(206.630, 0), new Vector(206.630, 55.5),
+                                new Vector(422.63, 0), new Vector(422.63, 55.5));
 
-    }
+                Polygon RendezvousZoneClose = new Polygon(new Vector(206.556, 121.414), new Vector(217.614, 116.591),
+                                new Vector(222.366, 127.61), new Vector(211.024, 206.847));
 
-    public Vector closestWallPoint(Vector p) {
-        return wall.closestPoint(p);
-    }
+                Polygon RendezvousOurTrench = new Polygon(new Vector(262.284, 255.531), new Vector(273.072, 251.062),
+                                new Vector(277.895, 262.121), new Vector(265.924, 267));
 
-    public Polygon[] getObstacles() {
-        return obstacles;
-    }
+                Polygon RendezvousTheirTrench = new Polygon(new Vector(351.365, 61.19), new Vector(362.22, 56.693),
+                                new Vector(367.043, 67.684), new Vector(355.957, 72.277));
+
+                Polygon RendezvousZoneFar = new Polygon(new Vector(407.148, 195.864), new Vector(411.617, 206.652),
+                                new Vector(422.731, 202.127), new Vector(418.139, 191.041));
+
+                Polygon SafeCloseOpponent = new Polygon(new Vector(0, 70.655 - ROBOT_WIDTH),
+                                new Vector(0, 118.655 + ROBOT_WIDTH), new Vector(30.072 + ROBOT_WIDTH, 94.655));
+
+                Polygon SafeFarOpponent = new Polygon(new Vector(FIELD_HEIGHT, 70.655 - ROBOT_WIDTH),
+                                new Vector(FIELD_HEIGHT, 130.655 + ROBOT_WIDTH),
+                                new Vector(599.187 - ROBOT_WIDTH, 100.655));
+
+                Polygon RendezvousOpponent = new Polygon(new Vector(362.561, 56.563), new Vector(208.711, 121.073),
+                                new Vector(244.992, 190.5), new Vector(384.288, 132.861));
+                obstacles[0] = RightSideTrench;
+                obstacles[1] = LeftSideTrench;
+
+                obstacles[2] = LeftCornerFar;
+                obstacles[3] = LeftCornerNear;
+                obstacles[4] = RightCornerFar;
+                obstacles[5] = RightCornerNear;
+
+                obstacles[6] = BlueTrench;
+
+                obstacles[7] = RendezvousZoneFar;
+                obstacles[8] = RendezvousOurTrench;
+                obstacles[9] = RendezvousTheirTrench;
+                obstacles[10] = RendezvousZoneClose;
+
+                obstacles[11] = SafeCloseOpponent;
+                obstacles[12] = SafeFarOpponent;
+
+                obstacles[13] = RendezvousOpponent;
+
+                Polygon SafeCloseAlliance = new Polygon(new Vector(0, 192.655 - SHORTEST_SIDE),
+                                new Vector(0, 252.655 + SHORTEST_SIDE), new Vector(30.72 + SHORTEST_SIDE, 119.655));
+
+                Polygon SafeFarAlliance = new Polygon(new Vector(599.187 - SHORTEST_SIDE, 228.655),
+                                new Vector(204.655 - SHORTEST_SIDE, 254.535 + SHORTEST_SIDE));
+
+                Polygon SafeTrench = new Polygon(new Vector(206.630, 267.811), new Vector(206.345, FIELD_WIDTH),
+                                new Vector(422.63, FIELD_WIDTH), new Vector(422.63, 267.811));
+
+                safeZones[0] = SafeCloseAlliance;
+                safeZones[1] = SafeFarAlliance;
+                safeZones[2] = SafeTrench;
+
+                VisionTarget farGoal = new VisionTarget(new Vector(FIELD_HEIGHT, 228.655), Math.PI, true);
+
+                visionTargets[0] = farGoal;
+
+        }
+
+        public Vector closestWallPoint(Vector p) {
+                return wall.closestPoint(p);
+        }
+
+        public Polygon[] getObstacles() {
+                return obstacles;
+        }
 
 }
