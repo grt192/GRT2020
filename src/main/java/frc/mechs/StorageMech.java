c6419745ce53428c28fadc8e890eab3461c2b443
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2020-02-04 18:37:16 -0800

    improve logic + lemonify

--- src/main/java/frc/mechs/StorageMech.java
+++ src/main/java/frc/mechs/StorageMech.java
@@ -1,17 +1,13 @@
 package frc.mechs;
 
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.Ultrasonic;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.ColorSensorV3;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.gen.BIGData;
 
 public class StorageMech implements Mech {
@@ -27,12 +23,13 @@ public class StorageMech implements Mech {
     private double RPM;
 
     // private final int range = 400;
-    private boolean seenBall, conveyerSeenBall = false;
+    private boolean seenLemon, conveyerSeenLemon, intakingLemon = false;
 
     private int lemonCount = 0;
     private int conveyerCount = 0;
 
-    private boolean ballsInCoveyer = false;
+    private boolean lemonsInCoveyer = false;
+    private boolean lemonInTop, lemonInMiddle, lemonInBottom = false;
 
     public StorageMech() {
         intake = new AnalogInput(BIGData.getInt("intake_analog"));
@@ -47,7 +44,7 @@ public class StorageMech implements Mech {
         ultrasonicRange = BIGData.getInt("ultrasonic_range");
         revRange = BIGData.getInt("rev_range");
         IRRange = BIGData.getInt("ir_range");
-        BIGData.put("ball_count", 3);
+        BIGData.put("lemon_count", 3);
     }
 
     private void setSpeeds(double RPM) {
@@ -59,40 +56,49 @@ public class StorageMech implements Mech {
     }
 
     public void update() {
-        setSpeeds(BIGData.getDouble("storage_rpm"));
+        // setSpeeds(BIGData.getDouble("storage_rpm"));
+        lemonInTop = top.getValue() < IRRange;
+        lemonInMiddle = middle.getValue() < IRRange;
+        lemonInBottom = bottom.getValue() < IRRange;
 
         // SmartDashboard.putNumber("Proximity Storage", storageDistance);
         // SmartDashboard.putNumber("Proximity Shooter", shooterDistance);
 
-        if (intake.getValue() < ultrasonicRange && !seenBall) {
+        if (intake.getValue() < ultrasonicRange && !intakingLemon) {
             lemonCount++;
+            intakingLemon = true;
         }
 
         if (intake.getValue() > ultrasonicRange) {
-            seenBall = true;
+            intakingLemon = false;
+            // seenLemon = true;
         }
         if (SHOOTER.getProximity() < revRange) {
             lemonCount--;
             conveyerCount--;
         }
 
-        if (bottom.getValue() < IRRange && !conveyerSeenBall) {
+        if (lemonInBottom && conveyerCount < 2) {
             conveyerCount++;
         }
-        if (bottom.getValue() > IRRange) {
-            conveyerSeenBall = true;
-        }
-        if (conveyerCount == 0 && middle.getValue() > IRRange) {
-            // TODO Spin Motor once
-        } else if (conveyerCount == 1 && middle.getValue() < IRRange && top.getValue() > IRRange) {
+
+        // if (bottom.getValue() > IRRange) {
+        // conveyerSeenLemon = true;
+        // }
+
+        if (conveyerCount == 0 && !lemonInMiddle) {
             // TODO Spin Motor once
         }
 
-        if (conveyerCount == 1 && top.getValue() < IRRange && middle.getValue() > IRRange) {
+        if (conveyerCount == 1 && !lemonInMiddle && !lemonInTop) {
+            // TODO Spin Motor once
+        }
+
+        if (conveyerCount == 1 && lemonInTop && !lemonInMiddle) {
             // TODO Spin Motor once backwards
         }
-        BIGData.put("ball_count", lemonCount);
-        SmartDashboard.putNumber("Lemon Count", BIGData.getInt("ball_count"));
+        BIGData.put("lemon_count", lemonCount);
+        SmartDashboard.putNumber("Lemon Count", BIGData.getInt("lemon_count"));
 
     }
 
